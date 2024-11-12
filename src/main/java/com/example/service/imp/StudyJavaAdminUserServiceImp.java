e781181078adcc456aa64ea79b6acf8751292621
xcy960815 <18763006837@163.com>
2024-11-12 20:45:18 +0800

    feat:代码暂存

--- src/main/java/com/example/service/imp/StudyJavaAdminUserServiceImp.java
+++ src/main/java/com/example/service/imp/StudyJavaAdminUserServiceImp.java
@@ -6,7 +6,7 @@ import com.example.domain.StudyJavaAdminUser;
 import com.example.mapper.StudyJavaAdminUserMapper;
 import com.example.service.StudyJavaAdminUserService;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,7 +21,7 @@ public class StudyJavaAdminUserServiceImp implements StudyJavaAdminUserService{
     @Override
     public int updateAdminUser(StudyJavaAdminUser studyJavaAdminUser){
         return studyJavaAdminUserMapper.updateAdminUser(studyJavaAdminUser);
-    };
+    }
 
     @Override
     public int insertAdminUser(StudyJavaAdminUser studyJavaAdminUser) {
