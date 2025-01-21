29d6b5b167dd4aff6e86823dfd709144b319e021
xcy960815 <18763006837@163.com>
2025-01-21 17:49:30 +0800

    feat: 杂项配置

--- src/main/java/com/example/controller/StudyJavaLoginController.java
+++ src/main/java/com/example/controller/StudyJavaLoginController.java
@@ -25,7 +25,7 @@ public class StudyJavaLoginController {
     @PostMapping("/logout")
     @ResponseBody
     public ResponseResult logout() {
-//        studyJavaLoginService.logout(studyJavaLoginParams);
+        // studyJavaLoginService.logout(studyJavaLoginParams);
         return ResponseGenerator.generatSuccessResult(null);
     }
 };
