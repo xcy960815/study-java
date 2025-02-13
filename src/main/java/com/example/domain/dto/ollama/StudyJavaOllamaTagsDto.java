2e23c1613da22576f73504abd77000fd0dccb14d
xcy960815 <18763006837@163.com>
2025-02-13 17:48:09 +0800

    feat: 开发ollama模块功能添加dto

---
+++ src/main/java/com/example/domain/dto/ollama/StudyJavaOllamaTagsDto.java
@@ -0,0 +1,32 @@
+package com.example.domain.dto.ollama;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class StudyJavaOllamaTagsDto {
+
+        private List<Model> models;
+
+        public static class Model {
+                private String name;
+                private String model;
+                private String modified_at;
+                private long size;
+                private String digest;
+                private Details details;
+
+                public static class Details {
+                        private String parent_model;
+                        private String format;
+                        private String family;
+                        private List<String> families;
+                        private String parameter_size;
+                        private String quantization_level;
+                }
+        }
+}
+
+
+
