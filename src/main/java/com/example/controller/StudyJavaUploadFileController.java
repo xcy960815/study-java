44623ce1e63d99d37609b2284211c1a2b1ee49d6
xcy960815 <18763006837@163.com>
2025-03-11 23:55:02 +0800

    feat: 大文件切片上传

--- src/main/java/com/example/controller/StudyJavaUploadFileController.java
+++ src/main/java/com/example/controller/StudyJavaUploadFileController.java
@@ -17,9 +17,9 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 @RestController
 public class StudyJavaUploadFileController {
-    private static final String UPLOAD_LARGE_DIR = "uploadLargeFiles"; // 存储路径
+    private static final String FOLDERS_LARGE_NAME = "uploadLargeFiles"; // 存储路径
     private static final ConcurrentHashMap<String, Integer> uploadedChunks = new ConcurrentHashMap<>(); // 记录已上传的分片
-    private static final String UPLOAD_DIR = "uploadFiles";
+    private static final String FOLDERS_NAME   = "uploadFiles";
     // 使用绝对路径避免相对路径问题
     private static final String BASE_PATH = System.getProperty("user.dir");
     /**
@@ -30,14 +30,14 @@ public class StudyJavaUploadFileController {
     @PostMapping("/uploadFile")
     public ResponseResult<String> uploadFile(@RequestParam("file") MultipartFile file) {
         try {
-            String dirPath = BASE_PATH + File.separator + UPLOAD_DIR;
-            log.info("dirPath {}",dirPath);
-            File dirFolder = new File(dirPath);
-            if (!dirFolder.exists()) {
-                dirFolder.mkdirs();
+            // 上传文件的地址
+            String foldersPath = BASE_PATH + File.separator + FOLDERS_NAME;
+            File folder = new File(foldersPath);
+            if (!folder.exists()) {
+                folder.mkdirs();
             }
             // 拼接文件路径
-            String targetFilePath = dirPath + File.separator + file.getOriginalFilename();
+            String targetFilePath = foldersPath + File.separator + file.getOriginalFilename();
             file.transferTo(new File(targetFilePath));
             return ResponseGenerator.generateSuccessResult(targetFilePath);
         } catch (IOException e) {
@@ -57,38 +57,41 @@ public class StudyJavaUploadFileController {
             @RequestParam("chunkIndex") int chunkIndex,
             @RequestParam("totalChunks") int totalChunks
     ) {
-        try {
-            File dir = new File(UPLOAD_LARGE_DIR);
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-            // 保存分片到临时文件
-            File chunkFile = new File(UPLOAD_LARGE_DIR, fileName + "_" + chunkIndex);
-            file.transferTo(chunkFile);
-
-            // 记录已上传分片
-            uploadedChunks.put(fileName + "_" + chunkIndex, 1);
-
-            // 检查是否所有分片都上传完成
-            if (uploadedChunks.size() == totalChunks) {
-                mergeFile(fileName, totalChunks);
-                 ResponseGenerator.generateSuccessResult("上传完成: " + fileName);
-            }
-
-             ResponseGenerator.generateSuccessResult("分片 " + chunkIndex + " 上传成功");
-        } catch (IOException e) {
+//        try {
+//            // 上传文件的地址
+//            String foldersLargePath = BASE_PATH + File.separator + FOLDERS_LARGE_NAME;
+//            File largeFolder = new File(foldersLargePath);
+//            if (!largeFolder.exists()) {
+//                largeFolder.mkdirs();
+//            }
+//            // 保存分片到临时文件
+//            File chunkFile = new File(FOLDERS_LARGE_NAME, fileName + "_" + chunkIndex);
+//            file.transferTo(chunkFile);
+//
+//            // 记录已上传分片
+//            uploadedChunks.put(fileName + "_" + chunkIndex, 1);
+//
+//            // 检查是否所有分片都上传完成
+//            if (uploadedChunks.size() == totalChunks) {
+//                mergeFile(fileName, totalChunks);
+//                 ResponseGenerator.generateSuccessResult("上传完成: " + fileName);
+//            }
+    log.info("uploadLargeFile {} {} {}",fileName,chunkIndex,totalChunks);
+//             ResponseGenerator.generateSuccessResult("分片 " + chunkIndex + " 上传成功");
+//        }
+        /*catch (IOException e) {
              ResponseGenerator.generateErrorResult("上传失败：" + e.getMessage());
-        }
+        }*/
     }
     /**
      * 合并分片文件
      */
     private void mergeFile(String fileName, int totalChunks) throws IOException {
-        File mergedFile = new File(UPLOAD_LARGE_DIR, fileName);
+        File mergedFile = new File(FOLDERS_LARGE_NAME, fileName);
         try (FileOutputStream fos = new FileOutputStream(mergedFile, true);
              BufferedOutputStream mergingStream = new BufferedOutputStream(fos)) {
             for (int i = 0; i < totalChunks; i++) {
-                File chunkFile = new File(UPLOAD_LARGE_DIR, fileName + "_" + i);
+                File chunkFile = new File(FOLDERS_LARGE_NAME, fileName + "_" + i);
                 Files.copy(chunkFile.toPath(), mergingStream);
                 chunkFile.delete(); // 删除分片文件
             }
